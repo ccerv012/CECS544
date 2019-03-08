ce4532ce102b3a190d8eb517fc5b7eaaa77ae18b
Chris Cervola <chriscervola@gmail.com>
2019-03-08 15:00:47 -0800

    fixed errors in the search results and delete functions

--- Bughound/Bugs.js
+++ Bughound/Bugs.js
@@ -14,23 +14,24 @@ function SearchBugs(){
     $.when(AJAX_Call).then(function (AJAX_Response){
         if (AJAX_Response['Result'] == 'Success') {
             // clear any existing search results
-            $('#bugSearchResults').empty()
+            $('#bugSearchResults').empty();
 
             //  creeate a table to dsiplay the results
             $('#bugSearchResults').append('<table id="BugResultsTable">');
 
             // add a header row to the table
-            $('#BugResultsTable').append('<thead><tr>Bug ID</tr><tr>Program</tr><tr>Report Type</tr><tr>Severity</tr><tr>Delete</tr></thead>');
+            $('#BugResultsTable').append('<thead><th>Bug ID</th><th>Program</th><th>Report Type</th><th>Severity</th><th>Delete</th></thead>');
         
             // loop through the search results and add them to the results table 
             var tr;
             for (var i = 0; i < AJAX_Response['Data'].length; i++) { 
                 tr = $('<tr/>'); // this is jquery short hand for adding a new row object
-                tr.append('<td onclick="OpenBugReport(\"' + AJAX_Response['Data'][i].ID + '\")" class="link">' + AJAX_Response['Data'][i].ID + '<td>'); // populate the new row, cell by cell
-                tr.append('<td>' + AJAX_Response['Data'][i].Program + '<td>'); // populate the new row, cell by cell
-                tr.append('<td>' + AJAX_Response['Data'][i].ReportType + '<td>'); // populate the new row, cell by cell
-                tr.append('<td>' + AJAX_Response['Data'][i].Severity + '<td>'); // populate the new row, cell by cell
-                tr.append('<td><button id="DeleteBug(\"' + AJAX_Response['Data'][i].ID + '\")"<td>'); // populate the new row, cell by cell
+                tr.append('<td onclick="OpenBugReport(\'' + AJAX_Response['Data'][i].ID + '\')" class="link">' + AJAX_Response['Data'][i].ID + '</td>'); // populate the new row, cell by cell
+                tr.append('<td>' + AJAX_Response['Data'][i].Program + '</td>'); // populate the new row, cell by cell
+                tr.append('<td>' + AJAX_Response['Data'][i].ReportType + '</td>'); // populate the new row, cell by cell
+                tr.append('<td>' + AJAX_Response['Data'][i].Severity + '</td>'); // populate the new row, cell by cell
+                tr.append('<td><button onclick="DeleteBug(\'' + AJAX_Response['Data'][i].ID + '\')">Delete</button></td>'); // populate the new row, cell by cell
+                $('#BugResultsTable').append(tr); // add the row you just built to the table
             }
         }
     })
@@ -43,7 +44,8 @@ function SearchBugs(){
 
 function DeleteBug(bugID){
     var params = {
-        'Method' : 'Delete'
+        'Method' : 'Delete',
+        'ID' : bugID
     }
 
     // data needs to be formatted before it can be sent via ajax
@@ -56,7 +58,8 @@ function DeleteBug(bugID){
     // wait for the ajax call to finish then execute...
     $.when(AJAX_Call).then(function (AJAX_Response){
         if (AJAX_Response['Result'] == 'Success'){
-            // remove the row from the table
+            // re-run the search to show the user its been deleted
+            SearchBugs();
 
             // let the user know it was successful
             alert('You have successfully deleted ' + bugID);
